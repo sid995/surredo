0149513d50d7663d66d2a675e54f4c2169f69487
Siddharth Kundu <sk31395@gmail.com>
2024-01-28 17:40:20 -0500

    auth, home layout, components and assets

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+
+import "../globals.css"
+
+export const metadata = {
+  title: 'Surredo',
+  description: 'A Next 14 Social Media Application'
+}
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <html lang="el">
+        <body className={`${inter.className} bg-dark-1`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
