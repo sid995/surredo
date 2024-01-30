1698ac5ca1d06c2105aedb7eece44ea980f57af4
Siddharth Kundu <sk31395@gmail.com>
2024-01-30 16:42:38 -0500

    BE implimentation for threads

---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,36 @@
+"use server"
+
+import { revalidatePath } from 'next/cache';
+import Thread from "@/lib/models/thread.model";
+import { connectToDB } from "@/lib/mongoose"
+import User from "../models/user.model";
+
+interface Params {
+  text: string,
+  author: string,
+  communityId: string | null,
+  path: string
+}
+
+export async function createThread({
+  text, author, communityId, path
+}: Params) {
+  try {
+
+    connectToDB()
+
+    const createdThread = await Thread.create({
+      text,
+      author,
+      community: null
+    });
+
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id }
+    })
+
+    revalidatePath(path)
+  } catch (error: any) {
+    throw new Error("Cannot create thread: ", error.message)
+  }
+}
